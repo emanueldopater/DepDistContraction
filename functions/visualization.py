2baf338fd465d1b2a39c4028a22b2f9ca5c197b3
Emanuel Dopater <emanueldopater@gmail.com>
2023-09-17 21:33:31 +0200

    Edit comments

--- functions/visualization.py
+++ functions/visualization.py
@@ -17,17 +17,17 @@ def pdf_gdf_after_n_iterations(
         file_prefix : str = ""
         ) -> None:
     """
-    Generates
+    Function which generates pdf plots and gdf files after specified number of iterations.
 
     Args:
-        G (nx.Graph): _description_
-        embedding_generator (DepDist_Base): _description_
-        iterations (int): _description_
-        show_iterations (list[int]): _description_
-        node_display_size_base (int, optional): _description_. Defaults to 1.
-        node_display_size_power (int, optional): _description_. Defaults to 1.9.
-        show_labels (bool, optional): _description_. Defaults to False.
-        file_prefix (str, optional): _description_. Defaults to "".
+        G (nx.Graph): NetworkX undirected graph. 
+        embedding_generator (DepDist_Base): Embedding generator.
+        iterations (int): Number of iterations to run.
+        show_iterations (list[int]): List of ints indicating which iterations to show.
+        node_display_size_base (int, optional): This paramter specify the size of node in plot. Defaults to 1.
+        node_display_size_power (float, optional): This paramter specify the size of node in plot. Defaults to 1.9.
+        show_labels (bool, optional): Whether to draw labels to plot. Defaults to False.
+        file_prefix (str, optional):File prexix. Defaults to "".
     """
 
     for i in range(iterations):
@@ -61,19 +61,29 @@ def pdf_gdf_after_n_iterations(
 
 
 
-
-
-
 def visualize_network_animation(
-        G: nx.Graph,
-        embedding_generator: DepDist_Base,
-        iterations: int,
-        visualization_step: int,
-        node_display_size_base: int = 1,
-        node_display_size_power: int = 1.9,
-         fps : int = 30,
-         show_labels : bool = False,
-         ):
+            G: nx.Graph,
+            embedding_generator: DepDist_Base,
+            iterations: int,
+            visualization_step: int,
+            node_display_size_base: int = 1,
+            node_display_size_power: int = 1.9,
+            fps : int = 30,
+            show_labels : bool = False,
+            ) -> None:
+    
+    """
+    Create animation of layout.
+
+    Args:
+        G (nx.Graph): NetworkX undirected graph. 
+        embedding_generator (DepDist_Base): Embedding generator.
+        iterations (int): Number of iterations to run.
+        node_display_size_base (int, optional): This paramter specify the size of node in plot. Defaults to 1.
+        node_display_size_power (float, optional): This paramter specify the size of node in plot. Defaults to 1.9.
+        fps (int, optional): Frames per second. Defaults to 30.
+        show_labels (bool, optional): Whether to draw labels to plot. Defaults to False.
+    """
     
     ### generate embeddings### 
     list_of_embs = []
@@ -128,38 +138,43 @@ def visualize_network_animation(
 
     
 
-def visualize_network_gif(G: nx.Graph, embs_list: list, output_path: str, scatter_size_offset: int = 2, scatter_size_degree_power: int = 2, fps : int = 30):
-    fig, ax = plt.subplots()
+# def visualize_network_gif(G: nx.Graph, 
+#                           embs_list: list, 
+#                           output_path: str, 
+#                           scatter_size_offset: int = 2, 
+#                           scatter_size_degree_power: int = 2, 
+#                           fps : int = 30):
+#     fig, ax = plt.subplots()
 
-    # Create empty scatter and plot objects
-    scatter = ax.scatter([], [])
-    edges, = ax.plot([], [], color='black', linewidth=0.1)
+#     # Create empty scatter and plot objects
+#     scatter = ax.scatter([], [])
+#     edges, = ax.plot([], [], color='black', linewidth=0.1)
 
-    def update(iteration):
-        ax.clear()
+#     def update(iteration):
+#         ax.clear()
 
-        # Get the node embeddings for the current iteration
-        node_embeddings = embs_list[iteration]
+#         # Get the node embeddings for the current iteration
+#         node_embeddings = embs_list[iteration]
 
-        # Update the scatter plot with the new node positions
-        scatter = ax.scatter(node_embeddings[:, 0], node_embeddings[:, 1], color='blue', s=[scatter_size_offset + G.degree[node] ** scatter_size_degree_power for node in sorted(G.nodes)])
-        for node in G.nodes:
-            node_emb = node_embeddings[node]
-            ax.annotate(str(node), (node_emb[0], node_emb[1]), color='red', fontsize=12)
+#         # Update the scatter plot with the new node positions
+#         scatter = ax.scatter(node_embeddings[:, 0], node_embeddings[:, 1], color='blue', s=[scatter_size_offset + G.degree[node] ** scatter_size_degree_power for node in sorted(G.nodes)])
+#         for node in G.nodes:
+#             node_emb = node_embeddings[node]
+#             ax.annotate(str(node), (node_emb[0], node_emb[1]), color='red', fontsize=12)
 
-        # Draw the edges
-        for edge in G.edges:
-            src, tar = edge
-            x = [node_embeddings[src][0], node_embeddings[tar][0]]
-            y = [node_embeddings[src][1], node_embeddings[tar][1]]
-            ax.plot(x, y, color='black', linewidth=0.1)
+#         # Draw the edges
+#         for edge in G.edges:
+#             src, tar = edge
+#             x = [node_embeddings[src][0], node_embeddings[tar][0]]
+#             y = [node_embeddings[src][1], node_embeddings[tar][1]]
+#             ax.plot(x, y, color='black', linewidth=0.1)
 
-        return scatter, edges
+#         return scatter, edges
 
-    # Create the animation
-    num_iterations = len(embs_list)
-    animation_fps = int(1000 / fps)
-    animation = FuncAnimation(fig, update, frames=num_iterations, interval=animation_fps)
+#     # Create the animation
+#     num_iterations = len(embs_list)
+#     animation_fps = int(1000 / fps)
+#     animation = FuncAnimation(fig, update, frames=num_iterations, interval=animation_fps)
 
-    # Show the animation
-    animation.save(output_path, writer='ffmpeg')
+#     # Show the animation
+#     animation.save(output_path, writer='ffmpeg')
